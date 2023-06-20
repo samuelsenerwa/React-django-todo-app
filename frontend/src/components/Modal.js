84336205c65d76df6bf1409880b2476cb3a03941
senerwa <samuelsenerwa>
2023-06-20 08:53:12 +0300

    modal class

---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react'
+
+import {
+    Buttton,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Form,
+    FormGroup,
+    Input,
+    Label,
+} from "reactstrap";
+
